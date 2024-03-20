363aebdb021abdd99aefac23c79586cabb6a48c4
XiaoInk <xiaoqiangink@outlook.com>
2024-03-20 16:37:06 +0800

    feat: 分类数据对接

--- src/api/RequestUrl.ts
+++ src/api/RequestUrl.ts
@@ -1,24 +1,24 @@
-  import BuildConfig from "../build/BuildConfig";
+import BuildConfig from "../build/BuildConfig";
 
-  //Home请求url
-  export const tabListUrl = BuildConfig.requestBaseUrl+"/v2/zero/arrange/menu/menuZero" //tab
-  export const tabContentUrl = BuildConfig.requestBaseUrl+"/v3/zero/arrange/layoutByMenuCode"
-  export const tabGetPlayUrl = BuildConfig.requestBaseUrl+"/v1/zero/recommend/assetdetail/"
+// Home请求url
+export const tabListUrl = BuildConfig.requestBaseUrl
+export const tabContentUrl = BuildConfig.requestBaseUrl + "/v3/zero/arrange/layoutByMenuCode"
+export const tabGetPlayUrl = BuildConfig.requestBaseUrl + "/v1/zero/recommend/assetdetail/"
 
-  //detail请求URL
-  export const mediaDetailUrl = BuildConfig.requestBaseUrl+"/v2/zero/media/assetlong/meta/"
-  export const mediaRecommendUrl = BuildConfig.requestBaseUrl+"/v1/zero/media/assetlong/recommend/"
-  export const episodeListUrl = BuildConfig.requestBaseUrl+"/v2/zero/media/assetlong/episodeList/"
-  export const episodePlayUrlUrl = BuildConfig.requestBaseUrl+"/v1/zero/media/assetlong/episode/playUrl/"
-  export const mediaAuthUrl = BuildConfig.requestBaseUrl+"/v2/zero/paid/accountAuth/authAssetLong"
-  export const episodeAuthUrl = BuildConfig.requestBaseUrl+"/v2/zero/paid/accountAuth/authAssetLongEpisode"
-  //filter请求URL
-  export const filterEntryUrl = BuildConfig.requestBaseUrl+"/v2/zero/tag/search/"
-  export const filterContentUrl = BuildConfig.requestBaseUrl+"/v1/zero/media/assetlong/search/assetlongByTags"
+// detail请求URL
+export const mediaDetailUrl = BuildConfig.requestBaseUrl + "/v2/zero/media/assetlong/meta/"
+export const mediaRecommendUrl = BuildConfig.requestBaseUrl + "/v1/zero/media/assetlong/recommend/"
+export const episodeListUrl = BuildConfig.requestBaseUrl + "/v2/zero/media/assetlong/episodeList/"
+export const episodePlayUrlUrl = BuildConfig.requestBaseUrl + "/v1/zero/media/assetlong/episode/playUrl/"
+export const mediaAuthUrl = BuildConfig.requestBaseUrl + "/v2/zero/paid/accountAuth/authAssetLong"
+export const episodeAuthUrl = BuildConfig.requestBaseUrl + "/v2/zero/paid/accountAuth/authAssetLongEpisode"
 
-  //search请求URL
-  export const hotSearchUrl=BuildConfig.requestBaseUrl+"/v1/zero/media/assetlong/search/keyword"
-  export const searchLongUrl=BuildConfig.requestBaseUrl+"/v1/zero/media/assetlong/search/assetlongByKeyword"
-  export const searchBookUrl = BuildConfig.requestBaseUrl +"/v1/zero/media/assetlong/search/pictureBookByKeyword"
-  export const searchShortUrl = BuildConfig.requestBaseUrl +"/v1/zero/media/search/assetByKeyword"
+// filter请求URL
+export const filterEntryUrl = BuildConfig.requestBaseUrl + "/v2/zero/tag/search/"
+export const filterContentUrl = BuildConfig.requestBaseUrl + "/v1/zero/media/assetlong/search/assetlongByTags"
 
+// search请求URL
+export const hotSearchUrl = BuildConfig.requestBaseUrl + "/v1/zero/media/assetlong/search/keyword"
+export const searchLongUrl = BuildConfig.requestBaseUrl + "/v1/zero/media/assetlong/search/assetlongByKeyword"
+export const searchBookUrl = BuildConfig.requestBaseUrl + "/v1/zero/media/assetlong/search/pictureBookByKeyword"
+export const searchShortUrl = BuildConfig.requestBaseUrl + "/v1/zero/media/search/assetByKeyword"
--- src/pages/home/build_data/tab/impl/Tab.ts
+++ src/pages/home/build_data/tab/impl/Tab.ts
@@ -1,31 +1,30 @@
-//
 export interface Tab {
-  //tab id
-  id: string
-  //tab 菜单code,用于获取对应tabContent数据
-  menuCode: string
-  //tab名称
-  menuName: string
-  //tab类型 0：文字，1：图片
-  menuType: string
-  //tab默认选中
-  defaultHome: string
-  //tab默认图片
-  image: string
-  //tab焦点图片
-  focusImage: string
-  //tab 选中图片
-  selectImage: string
-  //tab图片宽
-  imageWidth: string
-  //tab图片高
-  imageHeight: string
-  //角标图片
-  cornerImage: string
-  //焦点角标图片
-  focusCornerImage: string
-  //背景图
-  backgroundImage: string
-  //文字标题图标
-  textIcon?:string
+    //tab id
+    id?: string
+    //tab 菜单code,用于获取对应tabContent数据
+    menuCode: string
+    //tab名称
+    menuName: string
+    //tab类型 0：文字，1：图片
+    menuType: string
+    //tab默认选中
+    defaultHome?: string
+    //tab默认图片
+    image?: string
+    //tab焦点图片
+    focusImage?: string
+    //tab 选中图片
+    selectImage?: string
+    //tab图片宽
+    imageWidth?: string
+    //tab图片高
+    imageHeight?: string
+    //角标图片
+    cornerImage?: string
+    //焦点角标图片
+    focusCornerImage?: string
+    //背景图
+    backgroundImage?: string
+    //文字标题图标
+    textIcon?: string
 }
