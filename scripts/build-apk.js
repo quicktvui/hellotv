28c4a6c106cdf361f736ca03c0c12893b4567b5a
weipeng <weipeng@huan.tv>
2024-04-03 15:55:55 +0800

    fix(script):  windows编译问题

--- scripts/build-apk.js
+++ scripts/build-apk.js
@@ -2,4 +2,4 @@ const { exec } = require('shelljs');
 
 const buildType = process.argv.slice(2).join(' ');
 
-exec(`cd android && ./gradlew clean assemble${buildType}`)
+exec(`cd android && gradlew clean assemble${buildType}`)
