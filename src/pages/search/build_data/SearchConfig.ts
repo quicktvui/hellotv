a9a30f8db8f45e1b6850929eea45eb67d19974f5
xingrugeng <xrg3565119@>
2024-04-12 18:47:07 +0800

    feat:添加搜索到底提示

--- src/pages/search/build_data/SearchConfig.ts
+++ src/pages/search/build_data/SearchConfig.ts
@@ -3,7 +3,7 @@ export default {
   //搜索页搜索词条请求数量
   searchCenterPageSize:40,
   //搜索结果页请求数量
-  searchResultPageSize:12,
+  searchResultPageSize:18,
   //是否展示热门搜索/猜你想搜/搜索历史列表
   isShowCenterSearch:true,
   //左侧列表宽度
