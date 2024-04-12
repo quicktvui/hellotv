a5b4a588c1e6045889273bf8b672b1f4bd24901b
weipeng <weipeng@huan.tv>
2024-04-12 19:07:05 +0800

    update: 注释代码

--- android/app/src/main/java/com/quicktvui/hellotv/MainActivity.java
+++ android/app/src/main/java/com/quicktvui/hellotv/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.File;
-
 import eskit.sdk.core.EsData;
 import eskit.sdk.core.EsManager;
 
@@ -24,9 +22,9 @@ public class MainActivity extends AppCompatActivity {
         // 第一步 设置启动参数
         EsData data = DataCreateHelper.createFromAssets();
         // 另外还支持的加载方式:
-        DataCreateHelper.createFromFile(new File(""));
-        DataCreateHelper.createFromUrl("https://extcdn.hsrc.tv/data_center/files/plugin/2024/04/12/aa76e1a2-26b3-4a99-a16d-6bd1bd5a3da7.zip");
-        DataCreateHelper.createFromRepoServer("http://nexus.hmon.tv/");
+//        DataCreateHelper.createFromFile(new File(""));
+//        DataCreateHelper.createFromUrl("https://extcdn.hsrc.tv/data_center/files/plugin/2024/04/12/aa76e1a2-26b3-4a99-a16d-6bd1bd5a3da7.zip");
+//        DataCreateHelper.createFromRepoServer("http://nexus.hmon.tv/");
 
         // 第二步 启动
         EsManager.get().start(data);
