92c8c5f0e427036ebb112b677093e2640085e11b
liulipeng <liulipeng@huan.tv>
2024-04-17 11:25:08 +0800

    fix:详情页去掉按钮列表: ul不能在qt-waterfall中使用，会导致组件重建刷新两次

--- src/pages/detail/component/media-menu.vue
+++ src/pages/detail/component/media-menu.vue
@@ -1,38 +1,27 @@
 <template>
-  <qt-view class="media-menu-root-css"
+  <qt-row class="media-menu-root-css"
            :clipChildren="true">
-    <ul class="media-menu-root-list-css"
-        v-if="init"
-        :clipChildren="false"
-        :horizontal="true">
-      <li :clipChildren="false"
-          :key="index"
-          :type="1"
-          v-for="(item, index) in menuList">
-        <media-menu-button
-          v-if="item.type === 1"
-          ref="fullScreenButtonRef"
-          :icon="fullButtonNormal"
-          text="全屏"
-          :autofocus='autofocus'
-          @click="onFullButtonClick"
-          :vip-focus-icon="fullButtonVIPFocused"
-          :focus-icon="fullButtonFocused" />
+    <media-menu-button
+      ref="fullScreenButtonRef"
+      :icon="fullButtonNormal"
+      text="全屏"
+      :autofocus='autofocus'
+      @click="onFullButtonClick"
+      :vip-focus-icon="fullButtonVIPFocused"
+      :focus-icon="fullButtonFocused" />
 
-        <media-menu-vip-button
-          v-if="!authenticated && item.type === 2"
-          @click="onVIPButtonClick" />
+    <media-menu-vip-button
+      v-if="!authenticated"
+      @click="onVIPButtonClick" />
 
-        <media-menu-button
-          v-if="item.type === 3"
-          :focus-icon="favButtonFocused"
-          :icon="favButtonNormal"
-          text="收藏"
-          :vip-focus-icon="favButtonVIPFocused"
-          @click="onFavouriteButtonClick" />
-      </li>
-    </ul>
-  </qt-view>
+    <media-menu-button
+      :focus-icon="favButtonFocused"
+      :icon="favButtonNormal"
+      text="收藏"
+      :vip-focus-icon="favButtonVIPFocused"
+      @click="onFavouriteButtonClick" />
+
+  </qt-row>
 </template>
 
 <script lang="ts">
@@ -77,30 +66,20 @@ export default defineComponent({
     const eventbus = useESEventBus()
     let m: IMedia
 
-    const menuList = ref()
     const init = ref<boolean>(false)
 
     let autofocus = ref<boolean>(false)
 
     const fullScreenButtonRef = ref<Array<IMediaMenuButton>>()
 
-    const noVipMenuList = [
-      { type: 1 }, { type: 3 }, { type: 3 }, { type: 3 }, { type: 3 }, { type: 3 }
-    ]
-    const vipMenuList = [
-      { type: 1 }, { type: 2 }, { type: 3 }, { type: 3 }, { type: 3 }, { type: 3 }
-    ]
-
     watch(
       () => [mediaAuthorization?.value] as const,
       ([auth], [oldAuth]) => {
         if (mediaAuthorization?.value.auth) {
           authenticated.value = true
-          menuList.value = noVipMenuList
           init.value = true
         } else {
           authenticated.value = false
-          menuList.value = vipMenuList
           init.value = true
         }
       },
@@ -135,13 +114,11 @@ export default defineComponent({
     }
 
     function release(){
-      menuList.value = []
     }
 
     return {
       init,
       initMedia,
-      menuList,
       fullButtonFocused,
       fullButtonNormal,
       fullButtonVIPFocused,
