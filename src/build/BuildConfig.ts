b36773882974c08d7d6ea31293a719703db54cc1
XiaoInk <xiaoqiangink@outlook.com>
2024-05-07 19:11:49 +0800

    Merge branch 'dev-history' into dev-shell

--- src/build/BuildConfig.ts
+++ src/build/BuildConfig.ts
@@ -1,16 +1,17 @@
-const debug = true //true:输出日志模式，  false: 屏蔽日志模式
-const env = false // true:接口测试环境，false：接口正式环境
+const debug = true  // 调试模式
+const env = false   // true 测试环境、false 正式环境
+
 export default {
   debug: debug,
   testEnv: env,
   // 使用模拟数据
   useMockData: false,
-  //ES包名
+  // ES包名
   packageName: 'es.tv.huan.hellotv',
-  //域名
-  requestBaseUrl: process.env.requestBaseUrl || 'http://cms.hmon.tv/es_single/api',
-  //首页内容请求数量
-  tabContentPageSize: 10,
-  // 默认源
-  defaultSourceUrl: ''
+  // CMS
+  requestBaseUrl: process.env.requestBaseUrl || '',
+  // 首页内容请求数量
+  tabContentPageSize: 3,
+  // 直播源
+  defaultSourceUrl: process.env.defaultSourceUrl || ''
 }
