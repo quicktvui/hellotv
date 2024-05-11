11e380e123344964712feba748a4ba7176292182
“lushaojie” <1009636982@qq.com>
2024-05-11 09:38:53 +0800

    home

--- src/main-native.ts
+++ src/main-native.ts
@@ -12,7 +12,7 @@ import { createESADPlayer } from "@extscreen/es3-ad-player";
 
 //--------------------components-----------------------
 const routerOptions = {
-    main: 'activity2',
+    main: 'home',
     error: 'error',
     limit: 5,
     routes: routes,
