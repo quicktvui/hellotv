2ac77d61e6ae69b0bc0ab0532d557225bd11cf2c
XiaoInk <xiaoqiangink@outlook.com>
2024-05-22 11:45:14 +0800

    feat: 支持"我的"Tab可配置、修改Tab名称

--- src/pages/home/build_data/tab/TabTransferAdapter.ts
+++ src/pages/home/build_data/tab/TabTransferAdapter.ts
@@ -3,7 +3,7 @@ import {
   QTTabItem,
   QTTabItemType
 } from "@quicktvui/quicktvui3";
-import {ESListViewItemDecoration} from "@extscreen/es3-component";
+import { ESListViewItemDecoration } from "@extscreen/es3-component";
 import {
   buildQTTab,
   buildTabImgItem,
@@ -13,34 +13,46 @@ import {
   tabSectionTitleSize
 } from "./TabAdapter";
 import TabType from "./tab_type/TabType";
-import {Tab} from "./impl/Tab";
+import { Tab } from "./impl/Tab";
+import config from "../../config";
 
 /**
  * tab数据转换
  * @param tabs
  */
-export function buildTransferTabAdapter(tabs:Array<Tab>):QTTab{
-  const tabItemList: Array<QTTabItem> = []
+export function buildTransferTabAdapter(tabs: Array<Tab>): QTTab {
+  let length = tabs.length
   let defaultFocusIndex: number = 0
-  const length = tabs.length
-  tabs.map((tab,index)=>{
+
+  // 处理"我的"Tab
+  if (config.tab.showMineTab) {
+    length = tabs.unshift({
+      menuCode: '_mine',
+      menuName: config.tab.mineTabName,
+      menuType: '0'
+    })
+    defaultFocusIndex++
+  }
+
+  const tabItemList: Array<QTTabItem> = []
+  tabs.map((tab, index) => {
     if (tab.backgroundImage != null) {
       tabBackgroundUrls.set(tab.menuCode, tab.backgroundImage)
     }
     //设置默认选中tab
-    if (tab.defaultHome === '1'){
+    if (tab.defaultHome === '1') {
       defaultFocusIndex = index;
     }
     //设置左右距离边界间距
     const decoration: ESListViewItemDecoration = {
       left: index === 0 ? tabDecorationGapLeft : tabDecorationGap,
-      right: index === (length-1)?tabDecorationGapRight:0,
-      top:10
+      right: index === (length - 1) ? tabDecorationGapRight : 0,
+      top: 10
     }
-    const tabItem:QTTabItem = buildTabItem(tab,decoration)
+    const tabItem: QTTabItem = buildTabItem(tab, decoration)
     tabItemList.push(tabItem)
   })
-  const mTabs:QTTab = buildQTTab(defaultFocusIndex,defaultFocusIndex,tabItemList)
+  const mTabs: QTTab = buildQTTab(defaultFocusIndex, defaultFocusIndex, tabItemList)
   return mTabs
 }
 
@@ -49,23 +61,23 @@ export function buildTransferTabAdapter(tabs:Array<Tab>):QTTab{
  * @param tab 原始数据
  * @param decoration  tab item 左右间隔
  */
-function buildTabItem(tab:Tab,decoration?: ESListViewItemDecoration):QTTabItem{
+function buildTabItem(tab: Tab, decoration?: ESListViewItemDecoration): QTTabItem {
   const menuType = Number(tab.menuType)
-  let tabItem:QTTabItem
+  let tabItem: QTTabItem
   switch (menuType) {
     case TabType.TAB_TEXT_TYPE:
-      tabItem = buildTabTextItem(tab,tabSectionTitleSize,QTTabItemType.QT_TAB_ITEM_TYPE_DEFAULT,decoration)
+      tabItem = buildTabTextItem(tab, tabSectionTitleSize, QTTabItemType.QT_TAB_ITEM_TYPE_DEFAULT, decoration)
       break;
     case TabType.TAB_IMG_TYPE:
-      tabItem = buildTabImgItem(tab,1,decoration)
+      tabItem = buildTabImgItem(tab, 1, decoration)
       break;
     default:
-      tabItem = buildTabTextItem(tab,44,QTTabItemType.QT_TAB_ITEM_TYPE_DEFAULT,decoration)
+      tabItem = buildTabTextItem(tab, 44, QTTabItemType.QT_TAB_ITEM_TYPE_DEFAULT, decoration)
       break;
   }
   return tabItem
 }
 
-export function getTabBackground(tabId):string{
+export function getTabBackground(tabId): string {
   return <string>tabBackgroundUrls.get(tabId)
 }
---
+++ src/pages/home/config.ts
@@ -0,0 +1,8 @@
+export default {
+  tab: {
+    // 是否展示"我的"Tab
+    showMineTab: false,
+    // "我的"Tab展示名称
+    mineTabName: '我的'
+  }
+}
