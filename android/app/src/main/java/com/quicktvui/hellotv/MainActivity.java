ba19fa8ebe2a05434556b3f2ad032d4e6e4ffaa7
xxd <xuxiaodong@huan.tv>
2024-05-29 18:02:15 +0800

    feat:支持自定义启动图(gif)

--- android/app/src/main/java/com/quicktvui/hellotv/MainActivity.java
+++ android/app/src/main/java/com/quicktvui/hellotv/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
             finish();
             return;
         }
+        data.setCoverLayoutId(R.layout.my_custom_cover);
 
         // 第二步 启动
         EsManager.get().start(data);
