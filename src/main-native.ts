18325e8cb15d3dd6773f58a8e36aa335a4748574
lushaojie <1009636982@qq.com>
2024-08-06 21:02:09 +0800

    no

--- src/main-native.ts
+++ src/main-native.ts
@@ -14,7 +14,7 @@ import { createESADPlayer } from "@extscreen/es3-ad-player";
 
 //--------------------components-----------------------
 const routerOptions = {
-  main: "my", //ranking 排行榜 detail2 history my
+  main: "home", //ranking 排行榜 detail2 history my
   error: "error",
   limit: 5,
   routes: routes,
