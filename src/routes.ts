3e51b46b841beb2785a3a30f8dd3a5330a747c8a
xingrugeng <xrg3565119@>
2024-11-19 17:25:38 +0800

    feat: 修改分包

--- src/routes.ts
+++ src/routes.ts
@@ -1,29 +1,11 @@
 import home from './pages/home/index.vue'
-import detail from './pages/detail/index.vue'
-import introduction from './pages/introduction/index.vue'
 import {ESRouteType} from "@extscreen/es3-router";
-import screenList from './pages/filter/index.vue'
-import search from './pages/search/index.vue'
-import web from './pages/web/index.vue'
-import live from './pages/live/index.vue'
-import settings from './pages/settings/index.vue'
-import networkError from './components/NetworkError.vue'
-import history from './pages/history/history_template.vue'
-import login from './pages/login/index.vue'
-import activity2 from './pages/activity2/index.vue'
-import my from './pages/my/index.vue'
-import detail2 from './pages/detail2/index.vue'
-import D2Introduction from './pages/detail2/D2Introduction.vue'
-import ranking from './pages/ranking/index.vue'
-import shortVideo from './pages/shortVideo/index.vue'
-import test from './pages/test.vue'
-
 
 const routes = [
   {
     path: '/test',
     name: 'test',
-    component: test,
+    component: () => import('./pages/test.vue'),
   },
   {
     path: '/home',
@@ -33,62 +15,62 @@ const routes = [
   {
     path: '/series_view',
     name: 'series_view',
-    component: detail,
+    component: () => import('./pages/detail/index.vue'),
   },
   {
     path: '/introduction',
     name: 'introduction',
-    component: introduction,
+    component: () => import('./pages/introduction/index.vue'),
     type: ESRouteType.ES_ROUTE_TYPE_DIALOG
   },
   {
     path: '/screen_main_view',
     name: 'screen_main_view',
-    component: screenList,
+    component: () => import('./pages/filter/index.vue'),
   },
   {
     path: '/search',
     name: 'search',
-    component: search
+    component: () => import('./pages/search/index.vue')
   },
   {
     path: '/load_web_view',
     name: 'load_web_view',
-    component: web
+    component: () => import('./pages/web/index.vue')
   },
   {
-    name: 'live', path: '/live', component: live
+    name: 'live', path: '/live', component: () => import('./pages/live/index.vue')
   },
   {
-    name: 'activity2', path: '/activity2', component: activity2
+    name: 'activity2', path: '/activity2', component: () => import('./pages/activity2/index.vue')
   },
   {
-    name: 'settings', path: '/settings', component: settings, type: ESRouteType.ES_ROUTE_TYPE_DIALOG
+    name: 'settings', path: '/settings', component: () => import('./pages/settings/index.vue'), type: ESRouteType.ES_ROUTE_TYPE_DIALOG
   },
   {
-    name: 'network', path: '/network', component: networkError, type: ESRouteType.ES_ROUTE_TYPE_DIALOG
+    name: 'network', path: '/network', component: () => import('./components/NetworkError.vue'), type: ESRouteType.ES_ROUTE_TYPE_DIALOG
   },
   {
-    name: 'history', path: '/history', component: history
+    name: 'history', path: '/history', component: () => import('./pages/history/history_template.vue')
   },
   {
-    name:'login',path:'/login',component: login
+    name:'login',path:'/login',component: () => import('./pages/login/index.vue')
   },
   {
-    name: 'my', path: '/my', component: my
+    name: 'my', path: '/my', component: () => import('./pages/my/index.vue')
   },
   {
-    name: 'detail2', path: '/detail2', component: detail2
+    name: 'detail2', path: '/detail2', component: () => import('./pages/detail2/index.vue')
   },
   {
-    name: 'ranking', path: '/ranking', component: ranking
+    name: 'ranking', path: '/ranking', component: () => import('./pages/ranking/index.vue')
   }
   ,
   {
-    name: 'short_video', path: '/short_video', component: shortVideo
+    name: 'short_video', path: '/short_video', component: () => import('./pages/shortVideo/index.vue')
   },
   {
-    path: '/d2Introduction', name: 'd2Introduction', component: D2Introduction,
+    path: '/d2Introduction', name: 'd2Introduction', component: () => import('./pages/detail2/D2Introduction.vue'),
     type: ESRouteType.ES_ROUTE_TYPE_DIALOG
   },
 ]
