f054b06b678cad6db8ea52fac59e4b09030a6741
XiaoInk <xiaoqiangink@outlook.com>
2024-12-12 10:32:39 +0800

    feat: 筛选侧边栏

---
+++ src/pages/filter/components/sidebar/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <qt-view class="filter-sidebar">
+    <qt-list-view class="filter-sidebar-list" ref="listRef">
+      <qt-view class="filter-sidebar-list-item" style="background-color: red" :type="1" :focusable="false">
+        <qt-text
+          style="width: 320px; height: 106px; background-color: transparent; color: #ffffff; font-size: 40px"
+          text="电视剧"
+          gravity="center"
+          typeface="bold"
+        ></qt-text>
+      </qt-view>
+      <qt-view class="filter-sidebar-list-item" :type="2" :focusable="true">
+        <qt-text
+          class="filter-sidebar-list-item-text"
+          text="${title}"
+          gravity="center"
+          :focusable="false"
+          :duplicateParentState="true"
+        ></qt-text>
+      </qt-view>
+    </qt-list-view>
+  </qt-view>
+</template>
+
+<script setup lang="ts" name="FilterSidebar">
+import { ref } from 'vue'
+import { QTIListView } from '@quicktvui/quicktvui3'
+
+const listRef = ref<QTIListView>()
+
+function init() {
+  const listData: any[] = [{ type: 1, title: '电视剧' }]
+  for (let i = 1; i <= 10; i++) {
+    listData.push({
+      type: 2,
+      title: '武侠江湖' + i
+    })
+  }
+  listRef.value?.init(listData)
+}
+
+defineExpose({ init })
+</script>
+
+<style scoped>
+.filter-sidebar {
+  width: 320px;
+  height: 960px;
+  background-color: transparent;
+}
+
+.filter-sidebar-list {
+  width: 320px;
+  height: 960px;
+  background-color: transparent;
+}
+
+.filter-sidebar-list-item {
+  width: 320px;
+  height: 106px;
+  background-color: transparent;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  focus-background-color: #ffffff;
+}
+
+.filter-sidebar-list-item-text {
+  width: 320px;
+  height: 106px;
+  background-color: transparent;
+  color: rgba(255, 255, 255, 0.5);
+  font-size: 36px;
+  focus-color: #13161b;
+  select-color: #ffffff;
+}
+</style>
--- src/pages/filter/index.vue
+++ src/pages/filter/index.vue
@@ -6,6 +6,7 @@
     <qt-view class="filter-body" :clipChildren="true">
       <!-- 筛选列表扩展项 -->
       <!-- 筛选列表 -->
+      <filter-sidebar ref="sidebarRef" />
       <!-- 筛选内容 -->
       <filter-content ref="contentRef" />
     </qt-view>
@@ -17,16 +18,20 @@ import { ref } from 'vue'
 import { useESRouter } from '@extscreen/es3-router'
 import ThemeConfig from '../../config/theme-config'
 import TopView from '../../components/top-view.vue'
+import FilterSidebar from './components/sidebar/index.vue'
 import FilterContent from './components/content/index.vue'
 
 const router = useESRouter()
 
 // 背景色
 const bgColor = ThemeConfig.rootBgGradientColor
+// 筛选列表
+const sidebarRef = ref()
 // 筛选内容
 const contentRef = ref()
 
 function onESCreate() {
+  sidebarRef.value?.init()
   contentRef.value?.init()
 }
 
@@ -50,5 +55,6 @@ defineExpose({ onESCreate, onBackPressed })
   width: 1920px;
   height: 960px;
   background-color: transparent;
+  flex-direction: row;
 }
 </style>
