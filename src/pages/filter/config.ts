922492446de9fe6627703b965e3371644ad7ae24
XiaoInk <xiaoqiangink@outlook.com>
2024-12-24 15:12:33 +0800

    feat: 筛选左侧筛选项、右侧内容

--- src/pages/filter/config.ts
+++ src/pages/filter/config.ts
@@ -2,11 +2,11 @@ export default {
   // 是否展示左侧扩展列表
   isLeftListExpand: false,
   // 是否展示左侧筛选列表
-  isLeftList: false,
+  isLeftList: true,
   // 筛选条件每行高度
   listRowHeight: 66,
   // 筛选内容区域一行展示个数
-  gridSpanCount: 5,
+  gridSpanCount: 4,
   // 筛选内容分页大小
   gridContentLimit: 30
 }
---
+++ src/pages/filter/sidebarContents.vue
@@ -0,0 +1,85 @@
+<template>
+  <qt-view class="filter" :gradientBackground="{ colors: themeConfig.rootBgGradientColor, orientation: 4 }">
+    <!-- 顶部按钮 -->
+    <top-view name="topView" :logoRight="true" :nextFocusName="{ down: 'sidebarList' }" />
+    <!-- 内容主体 -->
+    <qt-view class="filter-body" :clipChildren="true">
+      <!-- 筛选列表 -->
+      <filter-sidebar ref="sidebarRef" :singleSelectPos="sidebarSinglePos" @onListItemFocused="onListItemFocused" />
+      <!-- 筛选内容 -->
+      <filter-content ref="contentRef" @setNextFocusNameRight="setNextFocusNameRight" />
+    </qt-view>
+  </qt-view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useESRouter } from '@extscreen/es3-router'
+import { buildFilters } from './adapter/index'
+import themeConfig from '../../config/theme-config'
+import TopView from '../../components/top-view.vue'
+import FilterSidebar from './components/sidebar/index.vue'
+import FilterContent from './components/content/index.vue'
+import filterManager from '../../api/filter/index'
+
+const router = useESRouter()
+
+// 筛选列表
+const sidebarRef = ref()
+const sidebarSinglePos = ref<number>(1)
+// 筛选内容
+const contentRef = ref()
+
+function onESCreate(params: { primaryId: string }) {
+  loadFilters(params.primaryId || '1848555233454727169')
+}
+
+function loadFilters(primaryId: string) {
+  filterManager.getFilters(primaryId).then((filters) => {
+    const { primaries, secondaries, tertiaries } = buildFilters(primaryId, filters)
+    // 初始化二级列表
+    sidebarRef.value?.init(secondaries)
+    // 初始化三级列表
+    contentRef.value?.init(tertiaries)
+  })
+}
+
+let lastPosition = sidebarSinglePos.value
+let listTimer: any = -1
+function onListItemFocused(evt) {
+  if (evt.isFocused && evt.position != lastPosition) {
+    clearTimeout(listTimer)
+    listTimer = setTimeout(() => {
+      lastPosition = evt.position
+      contentRef.value?.loadContents(evt.item.id, evt.item.type <= 2, evt.item.type === 9)
+    }, 300)
+  }
+}
+
+function setNextFocusNameRight(s: string) {
+  sidebarRef.value?.setNextFocusNameRight(s)
+}
+
+function onBackPressed() {
+  if (contentRef.value?.onBackPressed()) {
+    router.back()
+  }
+}
+
+defineExpose({ onESCreate, onBackPressed })
+</script>
+
+<style scoped>
+.filter {
+  width: 1920px;
+  height: 1080px;
+  background-color: transparent;
+}
+
+.filter-body {
+  width: 2130px;
+  height: 960px;
+  background-color: transparent;
+  flex-direction: row;
+}
+</style>
