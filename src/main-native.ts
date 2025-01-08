577466a0893737684f44780164b31dde5b1415ab
zhangliang <zhangliang@zhangliangdeMacBook-Pro.local>
2025-01-08 13:42:24 +0800

    详情页简介页面

--- src/main-native.ts
+++ src/main-native.ts
@@ -13,7 +13,7 @@ import { createESADPlayer } from "@extscreen/es3-ad-player";
 
 //--------------------components-----------------------
 const routerOptions = {
-  main: "home",
+  main: "detail",
   error: "error",
   limit: 5,
   routes: routes,
