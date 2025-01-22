60a654d5752223dbe53d662ec47c3dd89716b311
XiaoInk <xiaoqiangink@outlook.com>
2025-01-22 15:56:20 +0800

    .

--- src/pages/history/index.vue
+++ src/pages/history/index.vue
@@ -4,8 +4,8 @@
     <qt-view class="history-sidebar">
       <qt-text class="history-sidebar-tips" text="全部记录" gravity="center" :focusable="false"></qt-text>
       <qt-ul class="history-sidebar" :items="sidebarData">
-        <template #item="{ item }">
-          <qt-view class="history-sidebar-item" :type="1" :focusable="true" @focus="onSidebarItemFocus">
+        <template #item="{ index, item }">
+          <qt-view class="history-sidebar-item" :type="1" :focusable="true" @focus="(e) => onSidebarItemFocus(e, index)">
             <qt-text
               class="history-sidebar-item-text"
               :text="item.text"
@@ -27,11 +27,13 @@
       </qt-ul>
     </qt-view>
     <!-- 右侧列表 -->
-    <qt-view class="history-content" style="padding-left: 80px; padding-top: 106px">
-      <qt-ul class="history-content" :items="contentData" :spanCount="4" :clipChildren="false">
-        <template #item="{ item }">
+    <qt-view class="history-content" :clipChildren="true">
+      <!-- 右侧提示词 -->
+      <qt-text class="history-content-tips" text="按【菜单键】或长按【OK键】管理记录" gravity="center|end"></qt-text>
+      <qt-ul class="history-content-ul" :items="contentData" :spanCount="4" :clipChildren="false" :verticalFadingEdgeEnabled="true">
+        <template #item="{ index, item }">
           <!-- 常规 -->
-          <qt-view class="history-content-item" v-if="item.type === 1" :focusable="true" @click="onContentItemClick">
+          <qt-view class="history-content-ul-item" v-if="item.type === 1" :focusable="true" @click="onContentItemClick(index)">
             <qt-image
               style="width: 325px; height: 186px; background-color: transparent; border-radius: 16px"
               :src="item.image"
@@ -41,7 +43,7 @@
             ></qt-image>
             <qt-view style="height: 40px; background-color: transparent; margin-top: 11px" :focusable="false" :duplicateParentState="true">
               <qt-text
-                class="history-content-item-title"
+                class="history-content-ul-item-title"
                 :text="item.title"
                 gravity="center|start"
                 :showOnState="['normal', 'selected']"
@@ -49,7 +51,7 @@
                 :duplicateParentState="true"
               ></qt-text>
               <qt-text
-                class="history-content-item-title"
+                class="history-content-ul-item-title"
                 :text="item.title"
                 gravity="center|start"
                 :showOnState="'focused'"
@@ -58,16 +60,16 @@
               ></qt-text>
             </qt-view>
             <qt-text
-              class="history-content-item-progress"
+              class="history-content-ul-item-progress"
               :text="'看至 ' + item.progress"
               gravity="center|start"
               :focusable="false"
             ></qt-text>
           </qt-view>
           <!-- 到底提示 -->
-          <qt-view class="history-content-item-end" v-else :focusable="false">
+          <qt-view class="history-content-ul-item-end" v-else :focusable="false">
             <qt-text
-              class="history-content-item-end-text"
+              class="history-content-ul-item-end-text"
               text="已经到底啦，按【返回键】回到顶部"
               gravity="center"
               :focusable="false"
@@ -83,7 +85,7 @@
 import { ref, onMounted } from 'vue'
 import { useESToast } from '@extscreen/es3-core'
 import { QTListViewItem } from '@quicktvui/quicktvui3'
-import { numberToChinese } from '../../tools/index'
+import { buildMockData } from './mock'
 import themeConfig from '../../config/theme-config'
 
 const toast = useESToast()
@@ -92,36 +94,21 @@ const contentData = ref<QTListViewItem[]>([])
 
 onMounted(() => {
   sidebarData.value = [
-    { type: 1, itemSize: 106, text: '观看历史' },
-    { type: 1, itemSize: 106, text: '我的收藏' },
-    { type: 1, itemSize: 106, text: '已购内容' }
+    { type: 1, itemSize: 106, id: 1, text: '观看历史' },
+    { type: 1, itemSize: 106, id: 2, text: '我的收藏' },
+    { type: 1, itemSize: 106, id: 3, text: '已购内容' }
   ]
-
-  let arr: any = []
-  for (let i = 1; i <= 100; i++) {
-    arr.push({
-      type: 1,
-      itemSize: 186,
-      title: `第${numberToChinese(i)}回`,
-      progress: `${i}%`,
-      image: 'https://extcdn.hsrc.tv/channelzero_image/2023/12/15/5e84c591-038c-47d1-a266-46058d229d3b.png',
-      decoration: { right: 40, bottom: 40 }
-    })
-  }
-  arr.push({ type: 1000, itemSize: 100 })
-  contentData.value = arr
+  contentData.value = buildMockData()
 })
 
-function onSidebarItemFocus(evt) {
-  console.log('ok->focus', evt)
-  if (evt.isFocused) {
+function onSidebarItemFocus(e, index) {
+  if (e.isFocused) {
     toast.showToast('焦点事件')
   }
 }
 
-function onContentItemClick(evt) {
-  console.log('ok->click', evt)
-  toast.showToast('点击事件')
+function onContentItemClick(index) {
+  toast.showToast(`点击事件->${index}`)
 }
 </script>
 
