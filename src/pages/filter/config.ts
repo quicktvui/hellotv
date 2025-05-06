c795267b55683f847518500518afdeaf9e1e0de4
XiaoInk <xiaoqiangink@outlook.com>
2025-05-06 11:29:40 +0800

    feat: 筛选改为默认两栏竖图模式

--- src/pages/filter/config.ts
+++ src/pages/filter/config.ts
@@ -1,10 +1,10 @@
 export default {
   // 布局模式, 1 单页面、2 两栏、3 三栏
-  layoutMode: 3,
+  layoutMode: 2,
   // 筛选内容Item样式, 1 横图、2 竖图
-  gridItemMode: 1,
+  gridItemMode: 2,
   // 筛选内容区域一行展示个数
-  gridSpanCount: 4,
+  gridSpanCount: 5,
   // 筛选内容分页大小
   gridContentLimit: 60
 }
